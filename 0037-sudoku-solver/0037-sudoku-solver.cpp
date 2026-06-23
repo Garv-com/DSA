545a045683131e5f5dbfb1ad37b78cae17b5c7f9
Garv-com <garvsingh184@gmail.com>
2026-06-23 13:33:36 +0530

    Time: 233 ms (74.75%), Space: 8.9 MB (37.66%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+
+    bool isSafe(vector<vector<char>>& board, int row, int col, char dig){
+        //horizontal
+        for(int j=0; j<9; j++){
+            if(board[row][j]==dig) return false;
+        }
+
+        //vertical
+        for(int i=0; i<9; i++){
+            if(board[i][col]==dig) return false;
+        }
+
+        //grid
+        int rRow=(row/3)*3;
+        int rCol=(col/3)*3;
+        for(int i=rRow; i<=rRow+2; i++){
+            for(int j=rCol; j<=rCol+2; j++){
+                if(board[i][j]==dig) return false;
+            }
+        }
+
+        return true;
+    }
+
+    bool Sudoku(vector<vector<char>>& board, int row, int col){
+        if(row==9){
+            return true;
+        }
+
+        int nextRow=row, nextCol=col+1;
+        if(nextCol==9){
+            nextRow=row+1;
+            nextCol=0;
+        }
+
+        if(board[row][col]!='.'){
+            return Sudoku(board, nextRow, nextCol);
+        }
+
+        for(char dig='1'; dig<='9'; dig++){
+            if(isSafe(board, row, col, dig)){
+                board[row][col]=dig;
+                if(Sudoku(board, nextRow, nextCol)){
+                    return true;
+                }
+                board[row][col]='.';
+            }
+        }
+        return false;
+    }
+
+    void solveSudoku(vector<vector<char>>& board) {
+        Sudoku(board, 0, 0);
+    }
+};
