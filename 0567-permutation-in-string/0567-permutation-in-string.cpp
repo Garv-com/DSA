bb12e67496098c84f3395bd57cc581c4ad7c0a60
Garv-com <garvsingh184@gmail.com>
2026-06-23 13:41:26 +0530

    Time: 205 ms (17.81%), Space: 9.8 MB (98.55%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+
+    bool IsSame(int f1[], int f2[]){
+        for(int i=0; i<26; i++){
+            if(f1[i]!=f2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool checkInclusion(string s1, string s2) {
+        int freq[26]={0};
+        for(int i=0; i<s1.length(); i++){
+            freq[s1[i]-'a']++;
+        }
+
+        int windSize=s1.length();
+        for(int i=0; i<s2.length(); i++){
+            int windFreq[26]={0};
+            int windIdx=0, idx=i;
+            while(windIdx<windSize && idx<s2.length()){
+                windFreq[s2[idx]-'a']++;
+                windIdx++;
+                idx++;
+            }
+            if(IsSame(freq, windFreq)){
+            return true;
+        }
+        }
+
+        
+        return false;
+    }
+};
